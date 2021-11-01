f0001be87fc7dde7b1ea9ac7e14785464922a123
jacremer <jacremer@live.com>
2021-11-01 03:11:54 -0700

    proper rlib setup

--- config-override.js
+++ config-override.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-module.exports = function override(config, env) {
+module.exports = function override(config, _env) {
   const wasmExtensionRegExp = /\.wasm$/;
 
   config.resolve.extensions.push(".wasm");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,8 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
   darkMode: false,
-  theme: {
-    extend: {
-      colors: {
-        "purple-themed": {
-          DEFAULT: "#7B54FF",
-          light: "#2f2f49",
-        },
-        "green-themed": {
-          DEFAULT: "#70EC9D",
-        },
-      },
-    },
-  },
+  theme: {},
   variants: {},
   plugins: [],
 };
